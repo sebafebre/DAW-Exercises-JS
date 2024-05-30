18027ff32f0490617e83d0703508480c83026612
sebafebre <sebafebre5@gmail.com>
2024-05-30 20:01:19 -0300

    se agrego el HTML y exersices/variables_and_operators.js

---
+++ exercises/variables_and_operators.js
@@ -0,0 +1,22 @@
+// Ejercicio a: Crear dos variables numéricas y utilizar el operador suma para guardar el valor
+// de la suma de ambos números en una 3er variable.
+
+let num1 = 8;
+let num2 = 12;
+let suma = num1 + num2;
+console.log('La suma de num1 y num2 es:', suma);
+
+// Ejercicio b: Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+let string1 = "Hola";
+let string2 = "Mundo";
+let concatenacion = string1 + " " + string2;
+console.log('La union de string1 y string2 es:', concatenacion);
+
+// Ejercicio c: Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
+// guardando el resultado de la suma en una 3er variable (utilizar length).
+
+let string3 = "Programación";
+let string4 = "JavaScript";
+let sumaLongitud = string3.length + string4.length;
+console.log('La suma de la longitud de string3 y string4 es:', sumaLargos);
